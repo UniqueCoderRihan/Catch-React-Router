b4de7375e1f6008f7d827d1e382b1255969bdcaa
Rihanul Islam <rihanthedev@gmail.com>
2023-04-04 16:59:30 +0600

    food added

--- src/Components/Food/Food.jsx
+++ src/Components/Food/Food.jsx
@@ -4,10 +4,20 @@ const Food = ({food}) => {
     const {idMeal,strArea,strMealThumb,strMeal,strInstructions} = food;
     console.log(food);
     return (
-            <div className='bord'>
-                    <img src={strMealThumb} alt="" />
-                    <h3 className='text-4xl'>{strMeal}</h3>
-                </div>
+        <div className="card w-96 bg-base-100 shadow-xl m-5">
+        <figure><img src={strMealThumb} alt="FoodPic" /></figure>
+        <div className="card-body">
+          <h2 className="card-title">
+            {strMeal}
+            <div className="badge badge-secondary">NEW</div>
+          </h2>
+          <p>Fresh Food Forever Give From Us...</p>
+          <div className="card-actions justify-end">
+            <div className="badge badge-outline">Foods</div>
+            <div className="badge badge-outline">Fresh</div>
+          </div>
+        </div>
+      </div>
     );
 };
 
--- src/Components/Foods/Foods.jsx
+++ src/Components/Foods/Foods.jsx
@@ -6,8 +6,8 @@ const Foods = () => {
     const foods = useLoaderData();
     return (
         <div>
-            <h2>This KeyWord Available Foods {foods.meals.length}</h2>
-            <div>
+            <h2 className='text-6xl font-extralight text-center'>This KeyWord Available Foods {foods.meals.length}</h2>
+            <div className='grid md:grid-cols-3'>
                 {
                     foods.meals.map(food=> <Food
                     key={food.idMeal}
