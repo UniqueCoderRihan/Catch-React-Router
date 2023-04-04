8fd059578ab95bdbb50af4fd472c58bb050c0f4a
Rihanul Islam <rihanthedev@gmail.com>
2023-04-04 16:15:10 +0600

    commited

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import Header from "./Header/Header"
+import { Outlet } from "react-router-dom"
+import Header from "./Components/Header/Header"
 
 function App() {
     return (
         <div>
             <Header></Header>
+            <Outlet></Outlet>
         </div>
     )
 }
